1cacf734f4909006b12150b7bce1b74c65ff08ad
mincemaker <mincemaker@gmail.com>
2013-07-24 21:53:42 +0900

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TakamoriaikoApi::Application.config.secret_key_base = '6da6b0eed99ade7283463963fd6928bc3e24f3c3bec16855f0d6cb5bf49e547a721528934bea6358fa5158a64ddc8495def38023dc0497633c74d536ba33b31f'
