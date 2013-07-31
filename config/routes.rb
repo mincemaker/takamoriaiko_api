39821ea3688a374ac7f90c7ad179ae2578628ee6
mincemaker <mincemaker@gmail.com>
2013-08-01 01:13:54 +0900

    月末ガチャで高森さんがこなかった件について

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 TakamoriaikoApi::Application.routes.draw do
   root :to => "idols#index"
+  resources :idols, :only => [:index, :show], :format => :html
 
   scope "api" do
-    resources :idols, :only => [:index, :show], :defaults => {:format => :json}
+    resources :idols, :only => [:index, :show], :defaults => {:format => :json}, :as => :aiko_api
   end
-  resources :idols, :only => [:index, :show], :format => :html
 end
